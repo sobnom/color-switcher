72c99e2e450402ca978d899e5d63e512476fac6f
sobnom <sobnom.cse72@gmail.com>
2023-08-07 09:06:47 +0600

    color-switcher

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// document.querySelector('.color-switcher-btn').onclick = () =>{
+// document.querySelector('.color-switcher').classList.toggle('active');
+// };
+// let themeButtons = document.querySelectorAll('.theme-buttons');
+
+// themeButtons.forEach(color =>{
+//     color.addEventListener('click', () =>{
+//         let dataColor = color.getAttribute('data-color');
+//         document.querySelector(':root').style.setProperty('--main-color', dataColor);
+//     });
+// });
